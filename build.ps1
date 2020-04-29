1cb809926f12c7b5c8b070bb06d1ba542324fb4a
Robin Kaiser <t@r-k.mx>
2020-04-29 14:03:42 +0200

    changed build script

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $sourcecode = "main.go"
-$target = "build/HealthChecker"
-$env:GOOS = 'windows'; $env:GOARCH = 'amd64';               go build -o "$($target)_win64.exe" $sourcecode
-$env:GOOS = 'linux';   $env:GOARCH = 'amd64';               go build -o "$($target)_linux64" $sourcecode
-$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5;   go build -o "$($target)_raspi32" $sourcecode
-$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';               go build -o "$($target)_macos64.macos" $sourcecode
+$target = "build/health-checker"
+$env:GOOS = 'windows'; $env:GOARCH = 'amd64';               go build -o "$($target)-win64.exe" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'amd64';               go build -o "$($target)-linux64" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5;   go build -o "$($target)-raspi32" $sourcecode
+$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';               go build -o "$($target)-macos64.macos" $sourcecode
