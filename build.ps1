84bc03d360cd02a55c61cae462519062c6fed16d
Robin Kaiser <m@r-k.mx>
2021-11-26 09:18:10 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $sourcecode = "main.go"
 $target = "build/health-checker"
-$env:GOOS = 'windows'; $env:GOARCH = 'amd64';               go build -o "$($target)-win64.exe" $sourcecode
-$env:GOOS = 'linux';   $env:GOARCH = 'amd64';               go build -o "$($target)-linux64" $sourcecode
-$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5;   go build -o "$($target)-raspi32" $sourcecode
-$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';               go build -o "$($target)-macos64.macos" $sourcecode
+$env:GOOS = 'windows'; $env:GOARCH = 'amd64';               go build -o "$($target)-win64.exe"     -ldflags "-s -w" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'amd64';               go build -o "$($target)-linux64"       -ldflags "-s -w" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5;   go build -o "$($target)-raspi32"       -ldflags "-s -w" $sourcecode
+$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';               go build -o "$($target)-macos64.macos" -ldflags "-s -w" $sourcecode
